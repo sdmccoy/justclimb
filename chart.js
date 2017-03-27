5575a3454e0bfdea67d68e22065a686e2fe07656
Anthony Robinson <robinsonamk@gmail.com>
2017-03-27 10:26:06 -0700

    Chart linking working

--- chart.js
+++ chart.js
@@ -1,19 +1,29 @@
-
-google.charts.load('current', {packages: ['corechart']});
+google.charts.load('visualization', '1', {packages:['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-      // Define the chart to be drawn.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Element');
-  data.addColumn('number', 'Percentage');
-  data.addRows([
-        ['Climber 1', 0.70],
-        ['Climber 2', 0.20],
-        ['Climber 3', 0.10]
+  var data = google.visualization.arrayToDataTable([
+    ['Climber', 'link', 'Match Percentage'],
+    ['Climber 1', 'http://www.thefreedictionary.com/work', 11],
+    ['Climber 2', 'http://www.thefreedictionary.com/eat', 2],
+    ['Climber 3', 'http://www.thefreedictionary.com/commute', 2],
+    ['Climber 4', 'http://www.thefreedictionary.com/television',2],
+    ['Climber 5', 'http://www.thefreedictionary.com/sleep', 7]
   ]);
 
-      // Instantiate and draw the chart.
-  var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
-  chart.draw(data, null);
+  var view = new google.visualization.DataView(data);
+  view.setColumns([0, 2]);
+
+  var options = {
+    title: 'Climber Matches'
+  };
+
+  var chart = new google.visualization.PieChart(
+    document.getElementById('chart_div'));
+  chart.draw(view, options);
+
+  var selectHandler = function(e) {
+    window.location = data.getValue(chart.getSelection()[0]['row'], 1 );
+  };
+  google.visualization.events.addListener(chart, 'select', selectHandler);
 }
