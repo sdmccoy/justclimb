215148e872eb6d7e5519bac69ab4b6997a8b3fc9
Scott <sdmccoy@gmail.com>
2017-03-27 10:33:41 -0700

    Created the Formula in theory, Need to test

--- formula.js
+++ formula.js
@@ -4,27 +4,16 @@
 
 var gymLocationArray = ['Vertical World Seattle', 'Vertical World Redmond', 'Stone Gardens Seattle', 'Stone Gardens Bellevue', 'Seattle Bouldering Project'];
 
-function MatchGymLocation(){
-
-  if (elGymLocation) {
-    for (var i = 0; i < elgprofilesArr.length; i++) {
-      elGymLocation.match(profilesArr[i].location);
-
+function matchGymLocation(){
+  for (var i = 0; i < profilesArr.length; i++) {
+    var isMatch = elGymLocation.match(profilesArr[i].location);
+    if (isMatch) {
+      profilesArr[i].push(this.points + 6);
+    } else {
+      profilesArr[i].push(this.points + 0);
     }
   }
 };
 
-// trying a different if true statement.
-function MatchGymLocation(){
-  for (var i = 0; i < elgprofilesArr.length; i++) {
-    var isMatch = elGymLocation.match(profilesArr[i].location);
-    if (isMatch) {
-      profilesArr[i].points.push(this.points+6);
-    } else {
-      this.points+0;
-      }
-  }
-};
-
 var elGymLocation = document.getElementById('gym-location');
-elGymLocation.addEventListener('submit', MatchGymLocation);
+elGymLocation.addEventListener('submit', matchGymLocation);
