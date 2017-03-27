eeafad9efadfaba89631858a84f149e4fdc02b24
Scott <sdmccoy@gmail.com>
2017-03-27 16:48:04 -0700

    Skills function is working.  Need to tweak names and add event handler

--- location.js
+++ location.js
@@ -19,14 +19,14 @@ elGymLocation.addEventListener('submit', matchGymLocation);
 
 //console testing without event. This works in the for loop. Add the event up top to test.
 
-function matchGymLocation(){
-  for (var i = 0; i < profilesArr.length; i++) {
-    var newLocation = 'Seattle Bouldering Project';
-    var matching = profilesArr[i].location.includes(newLocation);
-    if (matching) {
-      profilesArr[i].points += 6;
-    } else {
-      profilesArr[i].points += 0;
-    }
-  }
-};
+// function matchGymLocation(){
+//   for (var i = 0; i < profilesArr.length; i++) {
+//     var newLocation = 'Seattle Bouldering Project';
+//     var matching = profilesArr[i].location.includes(newLocation);
+//     if (matching) {
+//       profilesArr[i].points += 6;
+//     } else {
+//       profilesArr[i].points += 0;
+//     }
+//   }
+// };
