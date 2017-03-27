14b43ac342079140e5d0fb522f118a67d9af6bd8
Scott <sdmccoy@gmail.com>
2017-03-27 10:26:20 -0700

    Built the formulas, not tested yet. Doing a merge party

--- formula.js
+++ formula.js
@@ -1,1 +1,30 @@
 'use strict';
+
+// working on gymlocation match formula
+
+var gymLocationArray = ['Vertical World Seattle', 'Vertical World Redmond', 'Stone Gardens Seattle', 'Stone Gardens Bellevue', 'Seattle Bouldering Project'];
+
+function MatchGymLocation(){
+
+  if (elGymLocation) {
+    for (var i = 0; i < elgprofilesArr.length; i++) {
+      elGymLocation.match(profilesArr[i].location);
+
+    }
+  }
+};
+
+// trying a different if true statement.
+function MatchGymLocation(){
+  for (var i = 0; i < elgprofilesArr.length; i++) {
+    var isMatch = elGymLocation.match(profilesArr[i].location);
+    if (isMatch) {
+      profilesArr[i].points.push(this.points+6);
+    } else {
+      this.points+0;
+      }
+  }
+};
+
+var elGymLocation = document.getElementById('gym-location');
+elGymLocation.addEventListener('submit', MatchGymLocation);
