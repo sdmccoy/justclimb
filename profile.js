bce1229d991d8c8fa7d884717b6a829891f81db7
Anthony Robinson <robinsonamk@gmail.com>
2017-03-28 14:54:31 -0700

    added local storage to app.js and profile.js

--- profile.js
+++ profile.js
@@ -1,9 +1,10 @@
-// filling user data into profile page.
-// function changeName(){
-// }
-// changeName();
-// console.log(changeName());
-// var profileName = document.getElementById('profile-name');
-// profileName.innertext = 'My new text!';
-// profileName.innertext = 'newName';
-  document.getElementById('profile-name').innerHTML = 'newName';
+//Get local storage for new user.
+var storedprofilesArr = JSON.parse(localStorage.getItem('profilesArr'));
+
+function populateNewUser(){
+  document.getElementById('profile-name').innerHTML = newName;
+  document.getElementById('gym-location').innerHTML = newLocation;
+  document.getElementById('climbing-type').innerHTML = [].concat(newSportTR, newSportLead, newBouldering);
+  document.getElementById('grade').innerHTML = newSkill;
+  document.getElementById('contact').innerHTML = newContact;
+}
