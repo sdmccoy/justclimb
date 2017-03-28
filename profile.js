b3e705c1e9a5800b59d244cd8a288a685974c962
Anthony Robinson <robinsonamk@gmail.com>
2017-03-28 15:18:19 -0700

    working on pushing stored array to profiles page

--- profile.js
+++ profile.js
@@ -1,10 +1,12 @@
-//Get local storage for new user.
+//Get local storage for new user. must navigate to match me page first...
 var storedprofilesArr = JSON.parse(localStorage.getItem('profilesArr'));
 
 function populateNewUser(){
-  document.getElementById('profile-name').innerHTML = newName;
-  document.getElementById('gym-location').innerHTML = newLocation;
-  document.getElementById('climbing-type').innerHTML = [].concat(newSportTR, newSportLead, newBouldering);
-  document.getElementById('grade').innerHTML = newSkill;
-  document.getElementById('contact').innerHTML = newContact;
+  // for (i = 0; i < storedprofilesArr; i++){
+    document.getElementById('profile-name').innerHTML = storedprofilesArr[0].name;
+    document.getElementById('gym-location').innerHTML = storedprofilesArr[0].gymLocation;
+    // document.getElementById('climbing-type').innerHTML = [].concat(newSportTR, newSportLead, newBouldering);
+    document.getElementById('grade').innerHTML = storedprofilesArr[0].skill;
+    document.getElementById('contact').innerHTML = storedprofilesArr[0].contact;
+  // }
 }
