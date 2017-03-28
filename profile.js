4ff0be5d858982b09d88a10d259eed9936e05d8b
Anthony Robinson <robinsonamk@gmail.com>
2017-03-28 15:46:34 -0700

    working on for loop to create new user divs and fill them

--- profile.js
+++ profile.js
@@ -1,12 +1,14 @@
 //Get local storage for new user. must navigate to match me page first...
 var storedprofilesArr = JSON.parse(localStorage.getItem('profilesArr'));
+var body = document.getElementsByTagName('body')[0];
 
 function populateNewUser(){
-  // for (i = 0; i < storedprofilesArr; i++){
-    document.getElementById('profile-name').innerHTML = storedprofilesArr[0].name;
-    document.getElementById('gym-location').innerHTML = storedprofilesArr[0].gymLocation;
-    // document.getElementById('climbing-type').innerHTML = [].concat(newSportTR, newSportLead, newBouldering);
-    document.getElementById('grade').innerHTML = storedprofilesArr[0].skill;
-    document.getElementById('contact').innerHTML = storedprofilesArr[0].contact;
-  // }
+  for (i = 0; i < storedprofilesArr; i++)
+  var newDiv = document.createElement('div');
+  document
+  document.getElementById('profile-name').innerHTML = storedprofilesArr[0].name;
+  document.getElementById('gym-location').innerHTML = storedprofilesArr[0].gymLocation;
+  // document.getElementById('climbing-type').innerHTML = [].concat(newSportTR, newSportLead, newBouldering);
+  document.getElementById('grade').innerHTML = storedprofilesArr[0].skill;
+  document.getElementById('contact').innerHTML = storedprofilesArr[0].contact;
 }
