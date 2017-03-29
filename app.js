e0e2c0e6d7fee54455cf7f87667ca8b7a4483f58
Scott <sdmccoy@gmail.com>
2017-03-29 15:57:00 -0700

    20x objects only created once if they are not in local storage

--- app.js
+++ app.js
@@ -5,27 +5,28 @@ var typeArr = ['Sport TR', 'Sport Lead', 'Bouldering'];
 var locationArr = ['Vertical World Seattle', 'Vertical World Redmond', 'Stone Gardens Seattle', 'Stone Gardens Bellevue', 'Seattle Bouldering Project'];
 var profilesArr = [];
 var globalPoints = [];
-
-var aJohnson = new Profile('Andrew Johnson', 'Vertical World Seattle', true, false, false, '5.5 - 5.7 / V0', 'andrew@johnson.com');
-var aMcguine = new Profile('Andrew McGuine', 'Vertical World Redmond', false, true, false, '5.10a - 5.10b / V2', 'andrew@mcguine.com');
-var aSparks = new Profile('Anthony Sparks', 'Stone Gardens Seattle', false, true, false, '5.10c - 5.10d / V3', 'anthony@sparks.com');
-var bMcarthur = new Profile('Brad Mcarthur', 'Stone Gardens Bellevue', true, false, false, '5.11a - 5.11b / V4', 'brad@mcarthur.com');
-var bMontegna = new Profile('Brad Montegna', 'Seattle Bouldering Project', false, false, true, '5.12c - 5.12d / V7', 'brad@montegna');
-var bCantrell = new Profile('Bryan Cantrell', 'Vertical World Redmond', true, true, false, '5.13c - 5.13d / V9', 'bryan@cantrell.com');
-var gOneill = new Profile('Grayson O\'neill', 'Seattle Bouldering Project', false, false, true, '5.11c - 5.11d / V5', 'grayson@oneill.com');
-var jDempsey = new Profile('Jason Dempsey', 'Vertical World Seattle', true, true, false, '5.12a - 5.12b / V6', 'jason@dempsey.com');
-var jTwanbly = new Profile('Jenna Twanbly', 'Stone Gardens Seattle', true, false, true, '5.12c - 5.12d / V7', 'jenna@twanbly.com');
-var jSmythe = new Profile('Jon Smythe', 'Vertical World Redmond', true, false, false, '5.5 - 5.7 / V0', 'jon@smythe.com');
-var jMartinez = new Profile('Jose Martinez', 'Stone Gardens Bellevue', true, true, false, '5.10a - 5.10b / V2', 'jose@martinez.com');
-var lAndrews = new Profile('Lance Andrews', 'Seattle Bouldering', false, false, true, '5.11a - 5.11b / V4', 'lance@andrews.com');
-var lBordain = new Profile('Leslie Bordain', 'Vertical World Seattle', true, true, true, '5.10c - 5.10d / V3', 'lance@bordain.com');
-var mLindell = new Profile('Marcus Lindell', 'Stone Gardens Seattle', true, true, false, '5.11c - 5.11d / V5', 'marcus@lindell.com');
-var mRyan = new Profile('Mike Ryan', 'Vertical World Redmond', false, true, false, '5.13c - 5.13d / V9', 'mike@ryan.com');
-var mLaughlin = new Profile('Milo Laughlin', 'Seattle Bouldering Project', false, false, true, '5.11a - 5.11b / V4', 'milo@laughlin.com');
-var nJohson = new Profile('Neil Johnson', 'Stone Gardens Bellevue', true, false, false, '5.10a - 5.10b / V2', 'neil@johnson.com');
-var rGarafalo = new Profile('Reid Garafalo', 'Vertical World Seattle', true, true, false, '5.12c - 5.12d / V7', 'reid@garafalo.com');
-var tLundell = new Profile('Teddy Lundell', 'Stone Gardens Seattle', true, false, true, '5.8 - 5.9 / V1', 'teddy@lundell.com');
-var tJanko = new Profile('Tony Janko', 'Vertical World Redmond', true, true, true, '5.11c - 5.11d / V5', 'tony@janko.com');
+function generateDBProfiles() {
+  var aJohnson = new Profile('Andrew Johnson', 'Vertical World Seattle', true, false, false, '5.5 - 5.7 / V0', 'andrew@johnson.com');
+  var aMcguine = new Profile('Andrew McGuine', 'Vertical World Redmond', false, true, false, '5.10a - 5.10b / V2', 'andrew@mcguine.com');
+  var aSparks = new Profile('Anthony Sparks', 'Stone Gardens Seattle', false, true, false, '5.10c - 5.10d / V3', 'anthony@sparks.com');
+  var bMcarthur = new Profile('Brad Mcarthur', 'Stone Gardens Bellevue', true, false, false, '5.11a - 5.11b / V4', 'brad@mcarthur.com');
+  var bMontegna = new Profile('Brad Montegna', 'Seattle Bouldering Project', false, false, true, '5.12c - 5.12d / V7', 'brad@montegna');
+  var bCantrell = new Profile('Bryan Cantrell', 'Vertical World Redmond', true, true, false, '5.13c - 5.13d / V9', 'bryan@cantrell.com');
+  var gOneill = new Profile('Grayson O\'neill', 'Seattle Bouldering Project', false, false, true, '5.11c - 5.11d / V5', 'grayson@oneill.com');
+  var jDempsey = new Profile('Jason Dempsey', 'Vertical World Seattle', true, true, false, '5.12a - 5.12b / V6', 'jason@dempsey.com');
+  var jTwanbly = new Profile('Jenna Twanbly', 'Stone Gardens Seattle', true, false, true, '5.12c - 5.12d / V7', 'jenna@twanbly.com');
+  var jSmythe = new Profile('Jon Smythe', 'Vertical World Redmond', true, false, false, '5.5 - 5.7 / V0', 'jon@smythe.com');
+  var jMartinez = new Profile('Jose Martinez', 'Stone Gardens Bellevue', true, true, false, '5.10a - 5.10b / V2', 'jose@martinez.com');
+  var lAndrews = new Profile('Lance Andrews', 'Seattle Bouldering', false, false, true, '5.11a - 5.11b / V4', 'lance@andrews.com');
+  var lBordain = new Profile('Leslie Bordain', 'Vertical World Seattle', true, true, true, '5.10c - 5.10d / V3', 'lance@bordain.com');
+  var mLindell = new Profile('Marcus Lindell', 'Stone Gardens Seattle', true, true, false, '5.11c - 5.11d / V5', 'marcus@lindell.com');
+  var mRyan = new Profile('Mike Ryan', 'Vertical World Redmond', false, true, false, '5.13c - 5.13d / V9', 'mike@ryan.com');
+  var mLaughlin = new Profile('Milo Laughlin', 'Seattle Bouldering Project', false, false, true, '5.11a - 5.11b / V4', 'milo@laughlin.com');
+  var nJohson = new Profile('Neil Johnson', 'Stone Gardens Bellevue', true, false, false, '5.10a - 5.10b / V2', 'neil@johnson.com');
+  var rGarafalo = new Profile('Reid Garafalo', 'Vertical World Seattle', true, true, false, '5.12c - 5.12d / V7', 'reid@garafalo.com');
+  var tLundell = new Profile('Teddy Lundell', 'Stone Gardens Seattle', true, false, true, '5.8 - 5.9 / V1', 'teddy@lundell.com');
+  var tJanko = new Profile('Tony Janko', 'Vertical World Redmond', true, true, true, '5.11c - 5.11d / V5', 'tony@janko.com');
+};
 
 var resetPoints = document.getElementById('reset-points');
 var name = document.getElementById('name');
@@ -33,6 +34,7 @@ var gymLocation = document.getElementById('gymLocation');
 var grade = document.getElementById('grade');
 var contact = document.getElementById('contact');
 
+// function generateDBProfiles() {
 function Profile(name, gymLocation, sportTR, sportLead, bouldering, skill, contact) {
   this.name = name;
   this.gymLocation = gymLocation;
@@ -44,8 +46,9 @@ function Profile(name, gymLocation, sportTR, sportLead, bouldering, skill, conta
   this.points = 0;
   profilesArr.push(this);
   globalPoints.push(this.points);
-  profilesArrToLocalStorage();
+    // profilesArrToLocalStorage();
 };
+// };
 
 //takes the form input and creates a new profile and pushes in to the empty profilesArr
 function newProfileFormSubmit(event) {
@@ -83,6 +86,10 @@ function newProfileFormSubmit(event) {
   matchBouldering();
   this.points = 0;
   var creatingNewProfile = new Profile(newName, newLocation, newSportTR, newSportLead, newBouldering, newSkill);
+  console.log(creatingNewProfile);
+  localStorage.setItem('creatingNewProfile', JSON.stringify(creatingNewProfile));
+  console.log(localStorage);
+  currentProfileArr.push(creatingNewProfile);
   window.location = 'chart.html';
   // drawChart();
   // console.log(this);
@@ -97,18 +104,54 @@ newProfile.addEventListener('submit', newProfileFormSubmit);
 
 //function to put the users form input into local storage
 function profilesArrToLocalStorage(){
-  localStorage.setItem('profilesArr', JSON.stringify(profilesArr));
+  console.log(JSON.parse(localStorage.getItem('profilesArr')));
+  if (JSON.parse(localStorage.getItem('profilesArr'))) {
+    var accumProfileArr = JSON.parse(localStorage.getItem('profilesArr'));
+    console.log(accumProfileArr);
+    console.log(currentProfileArr);
+    // accumProfileArr += currentProfileArr;
+    console.log(currentProfileArr);
+  } else {
+    generateDBProfiles();
+    console.log(localStorage);
+    localStorage.setItem('profilesArr', JSON.stringify(profilesArr));
+    console.log(localStorage);
+    // var accumProfileArr = JSON.parse(localStorage.getItem('profilesArr'));
+    // console.log(accumProfileArr);
+  }
+  // for (var i = 0; i < profilesArr.length; i++) {
+  //   totalProfileArr[i] = (accumProfileArr[i] || 0) + (currentProfileArr[i] || 0);
+  //   console.log(totalProfileArr);
+  // }
 };
+profilesArrToLocalStorage();
+
+var currentProfileArr = [];
+console.log(currentProfileArr);
+var accumProfileArr = [];
+console.log(accumProfileArr);
+var totalProfileArr = [];
+//
+// function addNewUsers(currentProfileArr) {
+//   if (JSON.parse(localStorage.getItem('profilesArr'))) {
+//     var accumUserProfileArr = JSON.parse(localStorage.getItem('profilesArr'));
+//   }
+//   for (var i = 0; i < Math.max(currentProfileArr.length, profilesArr.length); i++) {
+//     newestProfileArray[i] = ((currentProfileArr[i] || 0) + (profilesArr[i] || 0));
+//   }
+//   localStorage['profilesArr'] = JSON.stringify(newestProfileArray);
+//   return newestProfileArray;
+// };
 
 //working on storing the new profile without overwriting the previous new profile.
-var storedProfilesArr = JSON.parse(localStorage.getItem('profilesArr'));
+// var storedProfilesArr = JSON.parse(localStorage.getItem('profilesArr'));
 
-if (storedProfilesArr) {
-  for (var i = 0; i < storedProfilesArr.length; i++) {
+// if (storedProfilesArr) {
+  // for (var i = 0; i < storedProfilesArr.length; i++) {
     // storedProfilesArr.push()
-  }
-}
-var getStorageArr = localStorage.getItem('profilesArr', JSON.parse(profilesArr));
+  // }
+// }
+// var getStorageArr = localStorage.getItem('profilesArr', JSON.parse(profilesArr));
 // newProfileToLocalStorage();
 // filling user data into profile page.
 // function changeName(){
