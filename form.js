4da3d552549f9024dc231a70dee69e2234a62e0c
Anthony Robinson <robinsonamk@gmail.com>
2017-03-29 10:01:54 -0700

    fixed var i for matchSkill function

--- form.js
+++ form.js
@@ -1,10 +1,8 @@
 'use strict';
-
+var hideForm = document.getElementById('clickSet');
+hideForm.style.visibility = 'hidden';
 function hideResetButtonFunc() { //Match me "Click to find your Match! button hide function"
-  var hideForm = document.getElementById('clickSet');
   if (hideForm.style.visibility === 'hidden') {
     hideForm.style.visibility = 'visible';
-  } else {
-    hideForm.style.visibility = 'hidden';
   }
 }
