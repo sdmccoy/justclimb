aa09a65437ab927238e81406cb78fc59093a2ed6
Anthony Robinson <robinsonamk@gmail.com>
2017-03-28 17:18:27 -0700

    populating profiles page :)

--- profile.js
+++ profile.js
@@ -3,12 +3,32 @@ var storedprofilesArr = JSON.parse(localStorage.getItem('profilesArr'));
 var body = document.getElementsByTagName('body')[0];
 
 function populateNewUser(){
-  for (i = 0; i < storedprofilesArr; i++)
-  var newDiv = document.createElement('div');
-  document
-  document.getElementById('profile-name').innerHTML = storedprofilesArr[0].name;
-  document.getElementById('gym-location').innerHTML = storedprofilesArr[0].gymLocation;
-  // document.getElementById('climbing-type').innerHTML = [].concat(newSportTR, newSportLead, newBouldering);
-  document.getElementById('grade').innerHTML = storedprofilesArr[0].skill;
-  document.getElementById('contact').innerHTML = storedprofilesArr[0].contact;
-}
+  var newSection = document.createElement('section');
+  body.appendChild(newSection);
+  for (var i = 0; i < storedprofilesArr.length; i++){
+    var nameDiv = document.createElement('div');
+    nameDiv.innerText = storedprofilesArr[i].name;
+    newSection.appendChild(nameDiv);
+    var contactDiv = document.createElement('div');
+    contactDiv.innerText = storedprofilesArr[i].contact;
+    newSection.appendChild(contactDiv);
+    var gymDiv = document.createElement('div');
+    gymDiv.innerText = storedprofilesArr[i].gymLocation;
+    newSection.appendChild(gymDiv);
+    var skillDiv = document.createElement('div');
+    skillDiv.innerText = storedprofilesArr[i].skill;
+    newSection.appendChild(skillDiv);
+    var typeDiv = document.createElement('div');
+    typeDiv.innerText = storedprofilesArr[i].gymLocation;
+    newSection.appendChild(typeDiv);
+  }
+};
+
+  //
+  // var newDiv = document.createElement('div');
+  // document
+  // document.getElementById('profile-name').innerHTML = storedprofilesArr[0].name;
+  // document.getElementById('gym-location').innerHTML = storedprofilesArr[0].gymLocation;
+  // // document.getElementById('climbing-type').innerHTML = [].concat(newSportTR, newSportLead, newBouldering);
+  // document.getElementById('grade').innerHTML = storedprofilesArr[0].skill;
+  // document.getElementById('contact').innerHTML = storedprofilesArr[0].contact;
