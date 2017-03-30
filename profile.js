7f33f4feeee49df5fe9a4e64194d361c41caeef8
Zachary Diehl <zachary.diehl@gmail.com>
2017-03-30 14:42:25 -0700

    function is now working that sorts the profiles for best matches and outputs them in climber page onces user clicks on pie char

--- profile.js
+++ profile.js
@@ -2,7 +2,6 @@
 //Get local storage for new user. must navigate to match me page first...
 var storedprofilesArr = JSON.parse(localStorage.getItem('profilesArr'));
 var profileSection = document.getElementById('profile-info');
-var picturesArr = ['Assets/profile_Images/Andrew_Johnson.jpg', 'Assets/profile_Images/Andrew_McGuiness.jpg', 'Assets/profile_Images/Anthony_Sparks.jpg', 'Assets/profile_Images/Brad_Mcarthur.jpg', 'Assets/profile_Images/Brad_Montegna.jpg', 'Assets/profile_Images/Bryan_Cantrell.jpg', 'Assets/profile_Images/Grayson_O\'neill.jpg', 'Assets/profile_Images/Jason_Dempsey.jpg', 'Assets/profile_Images/Jenna_Twanbly.jpg', 'Assets/profile_Images/Jon_Smythe.jpg', 'Assets/profile_Images/Jose_Martinez.jpg', 'Assets/profile_Images/Lance_Andrews.jpg', 'Assets/profile_Images/Leslie_Bordain.jpg', 'Assets/profile_Images/Marcus_Lindell.jpg', 'Assets/profile_Images/Mike_Ryan.jpg', 'Assets/profile_Images/Milo_Laughlin.jpg', 'Assets/profile_Images/Neil_Johnson.jpg', 'Assets/profile_Images/Reid_Garafalo.jpg', 'Assets/profile_Images/Teddy_Lundell.jpg', 'Assets/profile_Images/Tony_Janko.jpg'];
 
 function populateNewUser(){
   for (var i = 0; i < storedprofilesArr.length; i++){
@@ -10,7 +9,7 @@ function populateNewUser(){
     newSection.className = 'userSection';
     profileSection.appendChild(newSection);
     var pictureImg = document.createElement('img');
-    pictureImg.setAttribute('src', picturesArr[i]);
+    pictureImg.setAttribute('src', storedprofilesArr[i].image);
     pictureImg.className = 'pictureImg';
     newSection.appendChild(pictureImg);
     var nameDiv = document.createElement('div');
