83ecd29d396e941ef14448cfb0a9310bc3517a8d
Anthony Robinson <robinsonamk@gmail.com>
2017-03-30 09:39:09 -0700

    added classnames to elements

--- profile.js
+++ profile.js
@@ -7,20 +7,26 @@ var picturesArr = ['Assets/Images/profile_Images/Andrew_Johnson.jpg', 'Assets/Im
 function populateNewUser(){
   for (var i = 0; i < storedprofilesArr.length; i++){
     var newSection = document.createElement('section');
+    newSection.className = 'userSection';
     profileSection.appendChild(newSection);
     var nameDiv = document.createElement('div');
+    nameDiv.className = 'profileDiv';
     nameDiv.innerText = 'Name: ' + storedprofilesArr[i].name;
     newSection.appendChild(nameDiv);
     var contactDiv = document.createElement('div');
+    contactDiv.className = 'profileDiv';
     contactDiv.innerText = 'Email: ' + storedprofilesArr[i].contact;
     newSection.appendChild(contactDiv);
     var gymDiv = document.createElement('div');
+    gymDiv.className = 'profileDiv';
     gymDiv.innerText = 'Gym Location: ' + storedprofilesArr[i].gymLocation;
     newSection.appendChild(gymDiv);
     var skillDiv = document.createElement('div');
+    skillDiv.className = 'profileDiv';
     skillDiv.innerText = 'Skill Level: ' + storedprofilesArr[i].skill;
     newSection.appendChild(skillDiv);
     var typeUL = document.createElement('ul');
+    typeUL.className = 'typeUL';
     newSection.appendChild(typeUL);
     typeUL.innerText = 'Climbing Type: ';
     if (storedprofilesArr[i].sportTR === true){
@@ -42,12 +48,3 @@ function populateNewUser(){
 };
 
 populateNewUser();
-
-  //
-  // var newDiv = document.createElement('div');
-  // document
-  // document.getElementById('profile-name').innerHTML = storedprofilesArr[0].name;
-  // document.getElementById('gym-location').innerHTML = storedprofilesArr[0].gymLocation;
-  // // document.getElementById('climbing-type').innerHTML = [].concat(newSportTR, newSportLead, newBouldering);
-  // document.getElementById('grade').innerHTML = storedprofilesArr[0].skill;
-  // document.getElementById('contact').innerHTML = storedprofilesArr[0].contact;
